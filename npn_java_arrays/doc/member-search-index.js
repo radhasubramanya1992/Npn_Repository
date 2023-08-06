db8d578fc2b96fa4b8bab8029e8ecd49e90d0e9e
radhasubramanya1992 <130886985+radhasubramanya1992@users.noreply.github.com>
2023-08-06 18:04:11 +0530

    Delete npn_java_arrays directory

--- npn_java_arrays/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.npn.test","c":"TestA","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.npn.test","c":"TestA","l":"TestA()","u":"%3Cinit%3E()"}];updateSearchResults();
--- npn_java_arrays/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.npn.test","l":"TestA"}];updateSearchResults();
