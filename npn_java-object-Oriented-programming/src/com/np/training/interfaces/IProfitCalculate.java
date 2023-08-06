cdd2b8c4b619b2e487a0202e8f5edfc90a086e57
radhasubramanya1992 <130886985+radhasubramanya1992@users.noreply.github.com>
2023-08-06 18:03:51 +0530

    Delete npn_java-object-Oriented-programming directory

--- npn_java-object-Oriented-programming/src/com/np/training/interfaces/IProfitCalculate.java
+++
@@ -1,7 +0,0 @@
-package com.np.training.interfaces;
-
-public interface IProfitCalculate 
-{
-	//public static final double a = 0;
-	public abstract double profitM(double sellingPrice,double costPrice);
-}
